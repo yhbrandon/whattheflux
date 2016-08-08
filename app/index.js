67bf7d9f6843c0e54ed9a168290bf346bfc17b99
Brandon Clark <yhbrandon@gmail.com>
2016-08-08 16:04:13 -0500

    Adding a repository to package.json, and some comments

--- app/index.js
+++ app/index.js
@@ -12,7 +12,9 @@ import routes from './core/routes'
 // Store
 import configureStore from './core/configureStore'
 
-const initialState = {}
+// Saw this on this boilerplate, thought it was cool
+// https://github.com/davezuko/react-redux-starter-kit
+const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState, browserHistory)
 const history = syncHistoryWithStore(browserHistory, store)
 
