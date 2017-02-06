676dcd3709e58a5cb1e1bed6b75bbe8e3bf6f4c8
Brandon Clark <yhbrandon@gmail.com>
2017-02-06 14:40:08 -0600

    Cleanup of bin

---
+++ bin/compile.js
@@ -0,0 +1,20 @@
+// Core
+import chalk from 'chalk'
+import webpack from 'webpack'
+
+// Configs
+import config from '../config'
+import webpackProdConfig from '../config/webpack/webpack.prod.config'
+
+const compiler = webpack(webpackProdConfig)
+
+compiler.run(error => {
+  if (error) {
+    console.log(chalk.red('[webpack] Cannot compile source...'))
+
+    return reject(error)
+  }
+
+  console.log(chalk.green('[webpack] Source compiled successfully...'))
+})
+
--- bin/server.js
+++ bin/server.js
@@ -1,32 +1,51 @@
 // Core
+import chalk from 'chalk'
 import express from 'express'
 import historyApiFallback from 'connect-history-api-fallback'
-import chalk from 'chalk'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 
-// Config
+// Configs
 import config from '../config'
+import webpackDevConfig from '../config/webpack/webpack.dev.config'
 
-// Servers
-import devServer from './dev.server'
+// Server setup
+console.log(chalk.yellow('[express] Initializing content...'))
 
 const app = express()
-
-// Setup server
-console.log(chalk.yellow('[express] Initializing content...'))
 
 app.use(express.static(config.paths.base))
 app.use(historyApiFallback({ verbose : true }))
 
-// Checking environment
 if (config.env === 'development') {
-  console.log(chalk.yellow('[webpack] Initializing development middlewares...'))
+  console.log(chalk.yellow('[webpack] Initializing development environment...'))
 
-  devServer(app, config)
+  const compiler = webpack(webpackDevConfig)
+  const { publicPath } = webpackDevConfig.output
+
+  const devMiddleWare = webpackDevMiddleware(compiler, {
+    publicPath,
+    contentBase: config.paths.app,
+    hot: true,
+    stats: {
+      children: false,
+      chunks : false,
+      chunkModules : false,
+      colors : true
+    }
+  })
+
+  app.use(devMiddleWare)
+  app.use(webpackHotMiddleware(compiler))
+} else {
+  console.log(chalk.red('[webpack] Cannot run a development server in a non development environment'))
 }
 
 app.listen(config.port, error => {
-  if (error)
+  if (error) {
     console.error(chalk.red(error))
-  else
+  } else {
     console.log(chalk.green(`[express] APP Listening at http://${config.host}:${config.port}`))
+  }
 })
